6ba7b5c2b06dd4fb647cbbf19c79a052ce037d06
unknown <profile2697@gmail.com>
2017-12-17 20:17:22 +0900

    thread에서 message queue를 이용한 view 갱신

--- ProcessActivity/mainthread/src/main/java/com/example/user/mainthread/MainActivity.java
+++ ProcessActivity/mainthread/src/main/java/com/example/user/mainthread/MainActivity.java
@@ -1,10 +1,26 @@
 package com.example.user.mainthread;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    updateTextView(msg.arg1);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     private class CountThread extends Thread {
         @Override
@@ -12,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
             for (int i = 0; i < 1000000; i++) {
                 Log.d("SeungMin", "TEST");
             }
+
+            Message msg = handler.obtainMessage();
+            msg.what = 0;
+            msg.arg1 = 941116;
+            handler.sendMessage(msg);
         }
     }
 
@@ -22,5 +43,10 @@ public class MainActivity extends AppCompatActivity {
 
         CountThread thread = new CountThread();
         thread.start();
+    }
+
+    private void updateTextView(int myBirth) {
+        TextView myBirthText = (TextView) MainActivity.this.findViewById(R.id.my_birth);
+        myBirthText.setText("나의 생일은: " + String.valueOf(myBirth));
     }
 }
