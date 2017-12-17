417ca82263469868181da684b23ddd5913744efa
unknown <profile2697@gmail.com>
2017-12-17 18:36:48 +0900

    save instance state test

--- ProcessActivity/backgroudprocess/src/main/java/com/example/user/backgroudprocess/MainActivity.java
+++ ProcessActivity/backgroudprocess/src/main/java/com/example/user/backgroudprocess/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.backgroudprocess;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            String state = savedInstanceState.getString("STATE");
+            Toast.makeText(this, "STATE = " + state, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Toast.makeText(this, "Saved state", Toast.LENGTH_LONG).show();
+        outState.putString("STATE", "FORCE STOP");
     }
 }
