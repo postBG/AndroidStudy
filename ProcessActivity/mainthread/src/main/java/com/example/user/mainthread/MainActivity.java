628b0880929ee4ab866a4267432519b60908651a
unknown <profile2697@gmail.com>
2017-12-17 19:06:51 +0900

    ANK 발생시키기

---
+++ ProcessActivity/mainthread/src/main/java/com/example/user/mainthread/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.user.mainthread;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for (int i = 0; i < 1000000; i++) {
+            Log.d("SeungMin", "TEST");
+        }
+    }
+}
